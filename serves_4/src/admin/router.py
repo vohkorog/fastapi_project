85884fb9089e1267633fab65b4ef2d180af3b5cb
kirill <kigo01@yandex.ru>
2026-06-25 22:10:06 +0300

    добавление возможности изменять пароль

--- serves_4/src/admin/router.py
+++ serves_4/src/admin/router.py
@@ -1,8 +1,8 @@
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from fastapi import APIRouter
-from src.admin.services import db
+from fastapi import APIRouter, Depends
+from src.admin.services import db, user
 from src.auth.services import user_db
 
 router = APIRouter(prefix='/admin', tags = ['DEBUG'])
@@ -19,3 +19,10 @@ def all_users():
     """Вывод всех пользователей"""
     users = user_db.get_all_users()
     return users
+
+
+@router.post('/change_pass')
+def change_pass(new_pass:str, old_pass:str, current_user: dict = Depends(user_db.get_current_user_from_token)):
+    
+    change = user.change_pass(user_login=current_user['login'] ,new_password=new_pass, old_password=old_pass, user_id=current_user['id'])
+    return change
--- serves_4/src/admin/services.py
+++ serves_4/src/admin/services.py
@@ -2,8 +2,10 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from models import Base
-from database import engine
+from models import Base, UserModel
+from database import engine, session_factory
+from ..security import verify_password, get_password_hash
+from ..auth.services import user_db
 
 class db:
     @staticmethod
@@ -13,3 +15,23 @@ class db:
     @staticmethod
     def delete_model():
         Base.metadata.drop_all(engine)
+
+class user:
+
+    @staticmethod
+    def change_pass(user_login:str, user_id: int, old_password: str, new_password: str):
+        user = user_db.get_user_by_login(user_login)
+        if not user:
+            return f'Неверный логин'
+        
+        if not verify_password(old_password, user.password_hash):
+            return 'Неверный пароль'
+        else:
+            with session_factory() as session:
+                new_password_hash = get_password_hash(new_password)
+                user = session.get(UserModel, user_id)
+                user.password_hash = new_password_hash
+                session.commit()
+                return {"id" : user.id,
+                        "login" : user.login,
+                        "email": user.email}
